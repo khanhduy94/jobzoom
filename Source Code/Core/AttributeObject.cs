4064153346ed0d4dda6a2fee8d0369c6bf334f2e
ldcphuc <ldcphuc@gmail.com>
2011-12-06 19:01:08 +0000

    Added the classes for Dynamic TagAttribute mapping feature

---
+++ Source Code/Core/AttributeObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace JobZoom.Core
+{
+    public class AttributeObject<T>
+    {
+        public Type Type { get; set; }
+        public T Attribute { get; set; }
+        public PropertyInfo Property { get; set; }
+    }
+}
---
+++ Source Code/Core/CoreModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\STUDY\QL092L\Subjects\Thesis\SVN\Source Code\Core\CoreModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
